91e4c33c3748cd0e7c3a8589ddf75131c7b754da
nome <jonathanmapaiva@gmail.com>
2026-05-18 20:42:49 -0300

    Atualização(feature)

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+class Calculadora:
+    def soma(self, n1, n2):
+        res = n1 + n2
+        return res
+
+    def subtracao(self, n1, n2):
+        res = n1 - n2
+        return res
+
+    def multiplicacao(self, n1, n2):
+        res = n1 * n2
+        return res
+
+    def divisao(self, n1, n2):
+
+        if n2 == 0:
+            raise ValueError("Não pode dividir por zero")
+
+        res = n1 / n2
+
+        return res
+
+    def potenciacao(self, n1, n2):
+        res = n1 ** n2
+        return res
